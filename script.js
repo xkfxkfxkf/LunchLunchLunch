223f5eb043c70f5a44d110e669319850e2b3f2ae
Jacky F <jackyfeng1992@gmail.com>
2018-06-24 23:13:35 +1000

    Added a 3 second countdown when displaying result.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let areaOfRestaurants = [
+var areaOfRestaurants = [
     'Pitt St Mall Upper Foodcourt', 'Pitt St Mall Lower Foodcourt', 'Hunter Connection', 
     'NAB Centre', 'MLC Centre'
 ];
 
-let beginButton = document.getElementById('beginButton');
-let result = document.getElementById('result');
+var beginButton = document.getElementById('beginButton');
+var result = document.getElementById('result');
+
+var countdownCounter = 3;
+//var resultCountdownInterval;
+
+function resultCountdown() {
+    result.innerText = countdownCounter + '...';
+    countdownCounter--;
+
+    if (countdownCounter == -1) {
+        result.innerText = areaOfRestaurants[Math.floor(Math.random() * areaOfRestaurants.length)];
+        clearInterval(resultCountdownInterval);
+        countdownCounter = 3;
+    }
+}
 
 beginButton.addEventListener('click', function() {
-    result.innerText = areaOfRestaurants[Math.floor(Math.random() * areaOfRestaurants.length)];
-    }
-);
+    resultCountdownInterval = setInterval(resultCountdown, 1000);
+    //If I declare resultCountdownInterval here, clearInterval doesn't work. If I remove the declaration but
+    //fail to declare it elsewhere, still works. Is this because Javascript just assumes I forgot to declare
+    //it and declares it for me?
+});
